db7e291cc248f94385ba471fc08751bb60be64c2
Yitangali <iyitnan07@gmail.com>
2026-02-05 15:02:40 +0700

    5th commit. Observation done

---
+++ application/libraries/Satusehat/Payloads/Observation/ObservationGigiGeligi.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH . 'libraries/Satusehat/Core/SatusehatPayload.php';
+
+class ObservationGigiGeligi extends SatusehatPayload
+{
+    protected $CI;
+    //protected $config;
+
+    public function __construct()
+    {
+        $CI =& get_instance();
+        $CI->load->library('Satusehat/Core/SatusehatConfig');
+        $CI->load->library('Satusehat/Core/SatusehatModel');
+        $this->templatePath = APPPATH . 'libraries/Satusehat/Payloads/Observation/ObservationGigiGeligi.json';
+        //$this->config = $this->CI->satusehatconfig->get('Org_id');
+    }
+
+    function getTodayIsoString(): string {
+        $dt = new DateTime('now', new DateTimeZone('UTC')); // pakai UTC
+        return $dt->format('Y-m-d\TH:i:sP');
+    }
+
+    public function build(array $data): array
+    {
+        //$orgId = $this->satusehatconfig->get('Org_id');
+        $payload = $this->loadTemplate();
+
+        $variables = [
+            "Patient_id"                        => $data['Patient_id'],
+            "Practitioner_id"                   => $data['Practitioner_id'],
+            "Encounter_id"                      => $data['Encounter_id'],
+            "Patient_Name"                      => $data['Patient_Name']
+        ];
+
+        return $this->replacePlaceholders($payload, $variables);
+    }
+}
+
