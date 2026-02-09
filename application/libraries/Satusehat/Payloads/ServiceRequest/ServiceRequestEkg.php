fbe5bc263b4e56d624dd22a5985f790ac9fd1588
Yitangali <iyitnan07@gmail.com>
2026-02-09 14:41:25 +0700

    7th commit. No. 14 done

---
+++ application/libraries/Satusehat/Payloads/ServiceRequest/ServiceRequestEkg.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH . 'libraries/Satusehat/Core/SatusehatPayload.php';
+
+class ServiceRequestEkg extends SatusehatPayload
+{
+    protected $CI;
+    //protected $config;
+
+    public function __construct()
+    {
+        $CI =& get_instance();
+        $CI->load->library('Satusehat/Core/SatusehatConfig');
+        $CI->load->library('Satusehat/Core/SatusehatModel');
+        $this->templatePath = APPPATH . 'libraries/Satusehat/Payloads/ServiceRequest/ServiceRequestEkg.json';
+        //$this->config = $this->CI->satusehatconfig->get('Org_id');
+    }
+
+    function getTodayIsoString(): string {
+        $dt = new DateTime('now', new DateTimeZone('UTC')); // pakai UTC
+        return $dt->format('Y-m-d\TH:i:sP');
+    }
+
+    public function build(array $data): array
+    {
+        //$orgId = $this->satusehatconfig->get('Org_id');
+        $payload = $this->loadTemplate();
+
+        $variables = [
+                'Org_id'                => $data['Org_id'],
+                'Patient_id'            => $data['Patient_id'],
+                'Encounter_id'          => $data['Encounter_id'],
+                'Practitioner_id'       => $data['Practitioner_id'],
+                'Practitioner_Name'     => $data['Practitioner_Name']
+        ];
+
+        return $this->replacePlaceholders($payload, $variables);
+    }
+}
+
